e76cbac495a34ab35abf5ebc6bda967369223026
arvoglade <arvoglade@gmail.com>
2025-01-22 20:46:41 +0500

    Account Controller

---
+++ src/main/java/ru/t1/java/demo/controller/TransactionController.java
@@ -0,0 +1,12 @@
+package ru.t1.java.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TransactionController {
+
+
+
+}
